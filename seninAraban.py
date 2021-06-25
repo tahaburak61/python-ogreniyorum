a2bfa3e65ebd90f188765b2b70bbd69f2d5e600b
Taha Burak <86467778+tahaburak61@users.noreply.github.com>
2021-06-25 14:45:04 +0300

    Araba satın alımı için yeterli miktar bulma kodu çalışmam

---
+++ seninAraban.py
@@ -0,0 +1,6 @@
+seninAraban = int(input())
+
+if(seninAraban <= 1000000):
+    print("bu arabayı satın almak için yeterli miktarda paran bulnmakta")
+else:
+    print("bu arabayı satın almak için yeterli miktarda paran bulunmamaktadır")
