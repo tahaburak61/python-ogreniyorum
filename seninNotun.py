7c2f713512746b7e3debb76dca59ebb3ceebe44c
Taha Burak <86467778+tahaburak61@users.noreply.github.com>
2021-06-25 14:38:41 +0300

    Sınav programları ile alakalı çalışmam

---
+++ seninNotun.py
@@ -0,0 +1,12 @@
+notun = int(input("bu dönem aldığın not"))
+
+if(notun >= 85 ):
+    print("beş puan aldınız")
+elif(notun >= 70 ):
+    print("dört puan aldınız")
+elif(notun >= 55 ):
+    print("üç puan aldınız")
+elif(notun >= 40 ):
+    print("iki puan aldınız")
+else:
+    print("bir puan aldınız")
