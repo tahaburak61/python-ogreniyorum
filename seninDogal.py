84c9bbc34d246541523b12eb374caa4112316c6a
Taha Burak <86467778+tahaburak61@users.noreply.github.com>
2021-06-25 14:37:01 +0300

    Sayılar ile ilgili çalışmam

---
+++ seninDogal.py
@@ -0,0 +1,8 @@
+seninDogal = int(input())
+
+if(seninDogal <= -1 ):
+    print("sayın negatif bir sayıdır")
+elif(seninDogal >= 1 ):
+    print("sayın pozitif bir sayıdır")
+elif(seninDogal == 0 ):
+    print("senin sayın sıfırdır")
