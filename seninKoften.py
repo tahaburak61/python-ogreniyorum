d52c0afbb56b26b500db4a0230108983fe02d399
Taha Burak <86467778+tahaburak61@users.noreply.github.com>
2021-06-25 14:38:08 +0300

    Köfteler ile ilgili çalışmam

---
+++ seninKoften.py
@@ -0,0 +1,26 @@
+seninKoften = input()
+
+if(seninKoften == "kaşarlı"):
+    print("kaşarlı köfte siparişi")
+elif(seninKoften == "domatesli"):
+    print("domatesli köfte siparişi")
+elif(seninKoften == "atom"):
+    print("atom köfte siparişi")
+elif(seninKoften == "acılı"):
+    print("acılı köfte siparişi")
+elif(seninKoften == "akcabat"):
+    print("akçabat köfte siparişi")
+elif(seninKoften == "ınegol kofte"):
+    print("inegöl köfte siparişi")
+elif(seninKoften == "acısız"):
+    print("acısız köfte siparişi")
+elif(seninKoften == "tekirdagi kofte"):
+    print("tekirdağ köfte siparişi")
+elif(seninKoften == "izmir"):
+    print("izmir köfte siparişi")
+elif(seninKoften == "mercimek"):
+    print("mercimek köfte siparişi")
+elif(seninKoften == "misket"):
+    print("misket köfte siparişi")
+elif(seninKoften == "hunkar"):
+    print("köfteli hünkar beğendi")
