370e29a3339fa485b0a57d2c523df6e77707fd46
Taha Burak <86467778+tahaburak61@users.noreply.github.com>
2021-06-25 14:39:02 +0300

    sayılarla büyük küçük adlı çalışmam

---
+++ seninSayin.py
@@ -0,0 +1,10 @@
+
+sayi_1 = int(input("ilk sayi:"))
+sayi_2 = int(input("ikinci sayi:"))
+
+if sayi_1 > sayi_2:
+    print("birinci sayi büyük")
+elif sayi_1 == sayi_2:
+    print("ikisi eşit")
+else:
+    print("ikinci sayi büyük")
