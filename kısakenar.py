5d2b02f6ab885f2b6837fd06e82dbc51ff54ecab
Taha Burak <86467778+tahaburak61@users.noreply.github.com>
2021-06-30 12:09:38 +0300

    dikdörtgenin alanını ve çevresini hesaplama adlı çalışmam

---
+++ kısakenar.py
@@ -0,0 +1,8 @@
+kısakenar = input("kısa kenarı giriniz")
+uzunkenar = input("uzun kenarı giriniz")
+
+cevre = (int(kısakenar) + int(uzunkenar)) 
+alan = int(kısakenar) * int(uzunkenar) 
+
+print("\nDikdörtgenin Çevresi : {0}".format(cevre))
+print("\nDikdörtgenin Alanı : {0}".format(alan))
