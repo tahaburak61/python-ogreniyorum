47c590822f8f471e8a85d98a4f210861c8162656
Taha Burak <86467778+tahaburak61@users.noreply.github.com>
2021-07-01 13:23:21 +0300

    python dort islem yaptik

---
+++ islem.py
@@ -0,0 +1,5 @@
+sayi1 = int(input("Toplanılacak 1.sayıyı giriniz"))
+sayi2 = int(input("Toplanılacak 2.sayıyı giriniz"))
+
+toplam = sayi1 - sayi2
+print("verdiğiniz sayıların toplamı" , toplam)    
