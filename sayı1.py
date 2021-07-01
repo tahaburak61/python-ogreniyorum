37e4308014ec3f1d3165be5bfd4cdeed46b55a46
Taha Burak <86467778+tahaburak61@users.noreply.github.com>
2021-07-01 13:21:17 +0300

    Delete sayı1.py

--- sayı1.py
+++
@@ -1,5 +0,0 @@
-sayi1 = int(input("Toplanılacak 1.sayıyı giriniz"))
-sayi2 = int(input("Toplanılacak 2.sayıyı giriniz"))
-
-toplam = sayi1 - sayi2
-print("verdiğiniz sayıların toplamı" , toplam)    
